6b8761e0ac145d0b1bc86d8aee3bb78bd9254d90
Edua Kaszas <edua.kaszas@usabilla.com>
2021-07-10 20:28:34 +0200

    Made Activities page more clean & simple

--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Activities.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBriefcase, faVolleyballBall, faUsers, faUtensils, faShoppingCart, faHeart, faBook } from '@fortawesome/free-solid-svg-icons'
